5c04cf61aa1ae8598d17fa32332f04029ac33279
Lucas Serafim <lucashenrique74148@gmail.com>
2025-03-12 10:21:50 -0300

    alterações

--- server.js
+++ server.js
@@ -9,9 +9,27 @@ app.use(express.static('public'));
 
 
 const musics = [
-  { id: 1, title: 'Bohemian Rhapsody', artist: 'Queen', duration: '5:55' },
-  { id: 2, title: 'Imagine', artist: 'John Lennon', duration: '3:03' },
-  { id: 3, title: 'Hey Jude', artist: 'The Beatles', duration: '7:11' }
+  { 
+    id: 1, 
+    title: 'Bohemian Rhapsody', 
+    artist: 'Queen', 
+    duration: '5:55',
+    cover: '/imgs/BohemiaRhaspody.jpg'  
+  },
+  { 
+    id: 2, 
+    title: 'Imagine', 
+    artist: 'John Lennon', 
+    duration: '3:03',
+    cover: '/imgs/Imagine.jpg'
+  },
+  { 
+    id: 3, 
+    title: 'Hey Jude', 
+    artist: 'The Beatles', 
+    duration: '7:11',
+    cover: '/imgs/HeyJude.jpg'
+  }
 ];
 
 
